daf1ad6d7321bce5f4f891cd614c9a6db38ed2f9
Laurent LEBRUN <laurent.lebrun@student.junia.com>
2023-10-23 09:53:57 +0200

    création fichier Robot header et cpp

---
+++ Robot.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include<vector>
+
+#include "Robot.h"
+#include "Environment.h"
+
+class Robot {
+public:
+    int id;
+    int pos_x;
+    int pos_y;
+    Robot(int id, Environment& map, int pos_x, int pos_y) {
+        this->id = id;
+        this->pos_x = pos_x;
+        this->pos_y = pos_y;
+        map.map_etat[pos_y][pos_x] = Etat::robot;
+    }
+
+    int get_id() {
+        return this->id;
+    }
+    int get_pos_x() {
+        return this->pos_x;
+    }
+    int get_pos_y() {
+        return this->pos_y;
+    }
+
+
+
+
+
+    void planter(Environment& map) {
+        //par défaut après avoir planter le robot se décale sur la droite
+        if (this->pos_x <= map.size_x - 2 and map.map_etat[this->pos_y][this->pos_x + 1] == Etat::libre) {
+            map.map_etat[this->pos_y][this->pos_x] = Etat::arbre;
+            map.map_etat[this->pos_y][this->pos_x + 1] = Etat::robot;
+            this->pos_x = this->pos_x + 1;
+        }
+        //si ce c'est pas possible, il monte
+        else if (this->pos_y >= 1 and map.map_etat[this->pos_y - 1][this->pos_x] == Etat::libre) {
+            map.map_etat[this->pos_y][this->pos_x] = Etat::arbre;
+            map.map_etat[this->pos_y - 1][this->pos_x] = Etat::robot;
+            this->pos_y = this->pos_y - 1;
+        }
+        //si ce n'est pas possible, il va vers la gauche
+        else if (this->pos_x >= 1 and map.map_etat[this->pos_y][this->pos_x - 1] == Etat::libre) {
+            map.map_etat[this->pos_y][this->pos_x] = Etat::arbre;
+            map.map_etat[this->pos_y][this->pos_x - 1] = Etat::robot;
+            this->pos_x = this->pos_x - 1;
+        }
+        //si ce n'est pas possible, il descent
+        else if (this->pos_y <= map.size_y - 2 and map.map_etat[this->pos_y + 1][this->pos_x] == Etat::libre) {
+            map.map_etat[this->pos_y][this->pos_x] = Etat::arbre;
+            map.map_etat[this->pos_y + 1][this->pos_x] = Etat::robot;
+            this->pos_y = this->pos_y + 1;
+        }
+        //si aucun de ces mouvement n'est possible alors le robot ne peut pas planter
+        else {
+            std::cout << "impossible de planter ici" << std::endl;
+        }
+
+    }
+
+
+
+
+
+
+
+    void deplacer(Environment& map, int dir) {
+        if (dir == 0) {
+            //déplacement vers le haut 
+            if (this->pos_y >= 1 and map.map_etat[this->pos_y - 1][this->pos_x] == Etat::libre) {
+                map.map_etat[this->pos_y - 1][this->pos_x] = Etat::robot;
+                map.map_etat[this->pos_y][this->pos_x] = Etat::libre;
+                this->pos_y = this->pos_y - 1;
+            }
+            else {
+                std::cout << "déplacement impossible" << std::endl;
+            }
+        }
+        else if (dir == 1) {
+            //déplacement vers la droite
+            if (this->pos_x <= map.size_x - 2 and map.map_etat[this->pos_y][this->pos_x + 1] == Etat::libre) {
+                map.map_etat[this->pos_y][this->pos_x + 1] = Etat::robot;
+                map.map_etat[this->pos_y][this->pos_x] = Etat::libre;
+                this->pos_x = this->pos_x + 1;
+            }
+            else {
+                std::cout << "déplacement impossible" << std::endl;
+            }
+        }
+        else if (dir == 2) {
+            //déplacement vers le bas
+            if (this->pos_y <= map.size_y - 2 and map.map_etat[this->pos_y + 1][this->pos_x] == Etat::libre) {
+                map.map_etat[this->pos_y + 1][this->pos_x] = Etat::robot;
+                map.map_etat[this->pos_y][this->pos_x] = Etat::libre;
+                this->pos_y = this->pos_y + 1;
+            }
+            else {
+                std::cout << "déplacement impossible" << std::endl;
+            }
+        }
+        else if (dir == 3) {
+            //déplacement vers la gauche
+            if (this->pos_x >= 1 and map.map_etat[this->pos_y][this->pos_x - 1] == Etat::libre) {
+                map.map_etat[this->pos_y][this->pos_x - 1] = Etat::robot;
+                map.map_etat[this->pos_y][this->pos_x] = Etat::libre;
+                this->pos_x = this->pos_x - 1;
+            }
+            else {
+                std::cout << "déplacement impossible" << std::endl;
+            }
+        }
+        else {
+            std::cout << "déplacement non reconnu" << std::endl;
+        }
+
+    }
+
+};
+
---
+++ Robot.h
@@ -0,0 +1,25 @@
+#include "Environment.h"
+
+class Robot {
+public:
+    int id;
+    int pos_x;
+    int pos_y;
+
+    Robot(int id, Environment& map, int pos_x, int pos_y);
+
+    int get_id();
+    int get_pos_x();
+    int get_pos_y();
+
+
+
+
+
+    void planter(Environment& map);
+
+    void deplacer(Environment& map, int dir);
+
+};
+
+#pragma once
