ae78df2f75e1afdb9bba2e7c5e7ea36e34605d53
Laurent LEBRUN <laurent.lebrun@student.junia.com>
2023-10-23 10:41:10 +0200

    programme main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
+#include "Environment.h"
+#include "afficheur.h"
+#include "Etat.h"
+#include "Robot.h"
 
-using namespace std;
+int main(int argc, char** argv) {
+    int n = 5;
+    Environment* map = new Environment(n, n);
+    Robot* robot1 = new Robot(1, *map, 0, 0);
+    std::cout << *map << std::endl;
+    for (int i = 0; i < (n * n) - 1; i++) {
+        robot1->planter(*map);
+    }
+    std::cout << *map << std::endl;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
 }
